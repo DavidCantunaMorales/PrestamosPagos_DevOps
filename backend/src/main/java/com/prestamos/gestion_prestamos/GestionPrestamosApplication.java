7e7c01417095fd9befe3dfcc86fce22cff13a8bc
MateoRoman <cmroman1@espe.edu.ec>
2025-06-14 21:09:48 -0500

    Cambios GestioPrestamosApplication.java

--- backend/src/main/java/com/prestamos/gestion_prestamos/GestionPrestamosApplication.java
+++ backend/src/main/java/com/prestamos/gestion_prestamos/GestionPrestamosApplication.java
@@ -1,8 +1,9 @@
 package com.prestamos.gestion_prestamos;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Aplicación principal de Spring Boot para la gestión de préstamos
 @SpringBootApplication
 public class GestionPrestamosApplication {
 
